4f5066760a778aa986c79fbd6d8d8e8841a0ff7d
artistjosh <artistjosh@users.noreply.github.com>
2019-10-17 16:55:11 +0000

    final update for 3

--- spec/03_classes_and_misc.spec.ts
+++ spec/03_classes_and_misc.spec.ts
@@ -17,3 +17,45 @@ describe('classes', () => {
         expect(stuff.PI).toBe(3.14);
     });
 });
+
+describe('miscellaneous', () => {
+    const numbers = [1, 2, 3, 4, 5];
+    it('immutably add an element to an array', () => {
+        const newNumbers = [0, ...numbers, 6];
+        expect(newNumbers).toEqual([0, 1, 2, 3, 4, 5, 6]);
+    });
+    it('imutably removing an element from an array', () => {
+        const newNumbers = numbers.filter(n => n !== 3);
+        expect(newNumbers).toEqual([1, 2, 4, 5]);
+    });
+
+    it('changing a property of an object immutably', () => {
+        const movie = { title: 'Episode IV: A New Hope', yearreleased: 1978 };
+
+        const newMovie = { ...movie, yearreleased: 1977 };
+
+        expect(newMovie.yearreleased).toBe(1977);
+        expect(movie.yearreleased).toBe(1978); // original object is still wrong!
+    });
+    it('array destructuring', () => {
+
+        // const first = numbers[0];
+        // const third = numbers[2];
+
+        const [first, , third] = numbers;
+        expect(first).toBe(1);
+        expect(third).toBe(3);
+
+    });
+    it('object destructuring', () => {
+        const movie = { title: 'Episode IV: A New Hope', yearreleased: 1977 };
+
+        // const title = movie.title;
+        // const year = movie.yearreleased;
+
+        const { title, yearreleased: year } = movie;
+
+        expect(title).toBe('Episode IV: A New Hope');
+        expect(year).toBe(1977);
+    });
+});
